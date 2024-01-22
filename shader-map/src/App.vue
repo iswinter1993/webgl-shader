842056afcf2fbd1fbd70c0e7a4feb5c7bd91574d
hand <iswinter@aliyun.com>
2024-01-22 16:15:39 +0800

    添加路由

--- shader-map/src/App.vue
+++ shader-map/src/App.vue
@@ -5,14 +5,22 @@
 <template>
   <RouterLink to="/" class="balllink">球</RouterLink>
   <RouterLink to="/img" class="imglink">图片</RouterLink>
+  <RouterLink to="/ball" class="waterlink">水球</RouterLink>
+  <RouterLink to="/mesh" class="imgballlink">图片球</RouterLink>
   <RouterView/>
 </template>
 
 <style scoped lang="less">
-.balllink,.imglink{
+.balllink,.imglink,.waterlink,.imgballlink{
 position: absolute;
 }
 .imglink{
   left: 30px;
+}
+.waterlink{
+  left: 90px;
+}
+.imgballlink{
+  left: 150px;
 }
 </style>
